3701f143195e7ac6ec902db92175922e16417b32
Dylan Wang <wyc6990906@hotmail.com>
2022-05-17 17:29:59 -0600

    kata6.js  WhereCanIPark

---
+++ KetaPractice/kata6.js
@@ -0,0 +1,55 @@
+const whereCanIPark = function (spots, vehicle) {
+  let final = [-1, -1];
+  const parkType = {
+    regular: ['R'],
+    small: ['R', 'S'],
+    motorcycle: ['R', 'S', 'M'],
+  };
+  for (let i = 0; i < spots.length; i++) {
+    const finalX = spots[i].findIndex(spot =>
+      parkType[vehicle].includes(spot)
+    );
+    if (finalX !== -1) {
+      final = [finalX, i];
+      break;
+    }
+  }
+  return final[0] === -1 ? false : final;
+
+};
+
+console.log(whereCanIPark(
+  [
+    // COLUMNS ARE X
+    // 0    1    2    3    4    5
+    ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+    ['s', 'M', 's', 'S', 'r', 'M'], // 1
+    ['s', 'M', 's', 'S', 'r', 'm'], // 2
+    ['S', 'r', 's', 'm', 'r', 'M'], // 3
+    ['S', 'r', 's', 'm', 'r', 'M'], // 4
+    ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+  ],
+  'regular'
+));
+
+console.log(whereCanIPark(
+  [
+    ['M', 'M', 'M', 'M'],
+    ['M', 's', 'M', 'M'],
+    ['M', 'M', 'M', 'M'],
+    ['M', 'M', 'r', 'M']
+  ],
+  'small'
+));
+
+console.log(whereCanIPark(
+  [
+    ['s', 's', 's', 's', 's', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['S', 'r', 's', 'm', 'r', 's'],
+    ['S', 'r', 's', 'm', 'R', 's'],
+    ['S', 'r', 'S', 'M', 'm', 'S']
+  ],
+  'motorcycle'
+))
