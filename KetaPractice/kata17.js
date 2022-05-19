694ca6b643218c210d7bb0eb2eca7b439576f0ec
Dylan Wang <wyc6990906@hotmail.com>
2022-05-19 13:09:44 -0600

    kata17  urlDecode

---
+++ KetaPractice/kata17.js
@@ -0,0 +1,24 @@
+const urlDecode = function (text) {
+  //first remove all the encode %20 characters and replace with spaces
+  let newStr = text.split('%20').join(' ');
+  //make an array of only the key value pairs
+  let keyArr = newStr.split('&');
+  let objArr = [];
+  let result = {};
+
+  //break apart the key value pairs into a new array or arrays of key and parameter
+  for (const key of keyArr) {
+    objArr.push(key.split('='));
+  }
+  for (const obj of objArr) {
+    result[obj[0]] = obj[1];
+  }
+
+  return result;
+};
+
+
+// console.log(urlDecode("duck=rubber"));
+// console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+// console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
